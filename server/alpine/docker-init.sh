179c0860ff37859eb10192469b1a07bad9ebac54
Adarsh Singhal <adarsh.singhal@outlook.com>
2023-07-06 10:28:39 +0530

    Update docker-init.sh
    Added docker run for DB

--- server/alpine/docker-init.sh
+++ server/alpine/docker-init.sh
@@ -1,2 +1,3 @@
 echo $DOCKER_PWD | docker login -u $DOCKER_USER --password-stdin
 docker pull $DOCKER_USER/as-pvt-repo:postgres-alpine3.18
+docker run --name nnt-db -p 5432:5432 -d adarshsinghal/as-pvt-repo:postgres-alpine3.18
