e262fbf7215e649aa49c44ad00f321403455d197
Adarsh Singhal <adarshsinghal695@gmail.com>
2023-07-06 22:21:21 +0530

    using sudo for starting docker daemon

--- server/alpine/docker-install.sh
+++ server/alpine/docker-install.sh
@@ -3,7 +3,7 @@ apk add docker
 sleep 5
 rc-update add docker default
 sleep 5
-service docker start > /tmp/docker_start.log 2>&1
+sudo service docker start
 rc-update add cgroups
 sleep 5
 apk add docker-compose
