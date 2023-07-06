11dee29075b0623f6a09fbb6c12f5796738a9951
Adarsh Singhal <adarshsinghal695@gmail.com>
2023-07-07 01:01:17 +0530

    fixed path

--- server/alpine/01-alpine-init.sh
+++ server/alpine/01-alpine-init.sh
@@ -3,15 +3,18 @@
 
 #--Local variables----------------------------------------------------
 GIT_REPO_URL=https://github.com/AdarshSinghal/scripts.git
-BASE_PATH=scripts/server/alpine
 WORK_DIR=/usr/local/git-repo
+
+BASE_PATH=./scripts/server/alpine
+SH1_PATH=$BASE_PATH/02-docker-install.sh
+SH2_PATH=$BASE_PATH/03-docker-cmd.sh
 #---------------------------------------------------------------------
 
 apk add git
 mkdir -p $WORK_DIR
 cd $WORK_DIR || exit
 git clone $GIT_REPO_URL
-chmod +x ./$BASE_PATH/02-docker-install.sh
-./$BASE_PATH/docker-install.sh
-chmod +x ./$BASE_PATH/03-docker-cmd.sh
-./$BASE_PATH/docker-init.sh
+chmod +x $SH1_PATH
+$SH1_PATH
+chmod +x $SH2_PATH
+$SH2_PATH
