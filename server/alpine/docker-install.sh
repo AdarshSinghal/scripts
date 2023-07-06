70f21eff6112be5e2f5450ff9fb4d3d340daec93
Adarsh Singhal <adarshsinghal695@gmail.com>
2023-07-06 22:11:57 +0530

    Added log for docker start

--- server/alpine/docker-install.sh
+++ server/alpine/docker-install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 apk add docker
-sleep 10
+sleep 5
 rc-update add docker default
-sleep 10
-service docker start
-sleep 10
+sleep 5
+service docker start > /tmp/docker_start.log 2>&1
 rc-update add cgroups
-sleep 10
+sleep 5
 apk add docker-compose
