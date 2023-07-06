ea9626cb840efcd7b46c4239b0efcc721c2b76d0
Adarsh Singhal <adarshsinghal695@gmail.com>
2023-07-06 22:46:56 +0530

    added sleep as docker start is async

--- server/alpine/docker-init.sh
+++ server/alpine/docker-init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 20s
 echo $DOCKER_PWD | docker login -u $DOCKER_USER --password-stdin
 docker pull $DOCKER_USER/as-pvt-repo:postgres-alpine3.18
 docker run --name nnt-db -p 5432:5432 -d adarshsinghal/as-pvt-repo:postgres-alpine3.18
