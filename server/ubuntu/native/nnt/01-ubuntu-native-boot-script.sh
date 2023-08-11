a4e9e300ac1e7bc0a901e19d72341e3032b79996
Adarsh Singhal <adarsh.singhal@outlook.com>
2023-08-11 18:43:13 +0530

    Added timestamp in logs (#7)
    Co-authored-by: Adarsh Singhal <adarshsinghal695@gmail.com>

--- server/ubuntu/native/nnt/01-ubuntu-native-boot-script.sh
+++ server/ubuntu/native/nnt/01-ubuntu-native-boot-script.sh
@@ -5,7 +5,9 @@ LOG_DIR=/var/log/my_script_logs
 INSTALLATION_LOG_FILE=$LOG_DIR/installation.log
 
 # Check if LOG_DIR is already defined in ~/.bashrc
-if ! grep -q "export LOG_DIR=$LOG_DIR" ~/.bashrc; then
+
+if ! [ -d "$LOG_DIR" ]; then
+  sudo mkdir $LOG_DIR
   {
     echo "export LOG_DIR=$LOG_DIR"
     echo "export INSTALLATION_LOG_FILE=$INSTALLATION_LOG_FILE"
@@ -13,37 +15,41 @@ if ! grep -q "export LOG_DIR=$LOG_DIR" ~/.bashrc; then
   . ~/.bashrc
 fi
 
-sudo mkdir $LOG_DIR
+
 ##### Completed -> Logging setup #####
 
-echo "Starting boot script execution with apt update." >> $INSTALLATION_LOG_FILE
+echo "$(date +'%Y-%m-%d %H:%M:%S') - Starting boot script execution with apt update." >> $INSTALLATION_LOG_FILE
 apt-get update
-echo "After apt update, Installing git." >> $INSTALLATION_LOG_FILE
+echo "$(date +'%Y-%m-%d %H:%M:%S') - After apt update, Installing git." >> $INSTALLATION_LOG_FILE
 apt-get install git -y
 
 SCRIPTS_DIR=/usr/local/git-repo/scripts
-echo "After installing git, checking whether dir $SCRIPTS_DIR exists." >> $INSTALLATION_LOG_FILE
+echo "$(date +'%Y-%m-%d %H:%M:%S') - After installing git, checking whether dir $SCRIPTS_DIR exists." >> $INSTALLATION_LOG_FILE
 
 if ! [ -d "$SCRIPTS_DIR" ]; then
-  echo "Creating dir $SCRIPTS_DIR" >> $INSTALLATION_LOG_FILE
+  echo "$(date +'%Y-%m-%d %H:%M:%S') - Creating dir $SCRIPTS_DIR" >> $INSTALLATION_LOG_FILE
   mkdir -p $SCRIPTS_DIR
 fi
 cd $SCRIPTS_DIR || exit
-echo "Switched to dir $SCRIPTS_DIR." >> $INSTALLATION_LOG_FILE
+echo "$(date +'%Y-%m-%d %H:%M:%S') - Switched to dir $SCRIPTS_DIR." >> $INSTALLATION_LOG_FILE
 
 if [ -d "$SCRIPTS_DIR/.git" ]; then
-    git pull
-    echo "Git repository inside $SCRIPTS_DIR has been updated." >> $INSTALLATION_LOG_FILE
+  rm -r "$SCRIPTS_DIR/.git"
+    if git pull; then
+        echo "$(date +'%Y-%m-%d %H:%M:%S') - Git repository inside $SCRIPTS_DIR has been updated." >> "$INSTALLATION_LOG_FILE"
+    else
+        echo "$(date +'%Y-%m-%d %H:%M:%S') - Error updating Git repository." >> "$INSTALLATION_LOG_FILE"
+    fi
 else
     git clone https://github.com/AdarshSinghal/scripts.git "$SCRIPTS_DIR"
-    echo "Git repository has been cloned into $SCRIPTS_DIR" >> $INSTALLATION_LOG_FILE
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - Git repository has been cloned into $SCRIPTS_DIR" >> $INSTALLATION_LOG_FILE
 fi
 
 ENV_SCRIPT_DIR=$SCRIPTS_DIR/server/ubuntu/native/nnt
 
 SCRIPT_JDK_MVN_INSTALL=$ENV_SCRIPT_DIR/installation-jdk-mvn.sh
 chmod +x $SCRIPT_JDK_MVN_INSTALL
-echo "Executing $SCRIPT_JDK_MVN_INSTALL" >> $INSTALLATION_LOG_FILE
+echo "$(date +'%Y-%m-%d %H:%M:%S') - Executing $SCRIPT_JDK_MVN_INSTALL" >> $INSTALLATION_LOG_FILE
 $SCRIPT_JDK_MVN_INSTALL
 
 
--- server/ubuntu/native/nnt/installation-jdk-mvn.sh
+++ server/ubuntu/native/nnt/installation-jdk-mvn.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 . ~/.bashrc
-echo "Installing openjdk-17-jdk maven" >> "$INSTALLATION_LOG_FILE"
+echo "$(date +'%Y-%m-%d %H:%M:%S') - Installing openjdk-17-jdk maven" >> "$INSTALLATION_LOG_FILE"
 sudo apt install openjdk-17-jdk maven -y
 
 if ! grep -q "export JAVA_HOME" ~/.bashrc; then
-echo "Setting env var for openjdk-17-jdk maven" >> "$INSTALLATION_LOG_FILE"
+echo "$(date +'%Y-%m-%d %H:%M:%S') - Setting env var for openjdk-17-jdk maven" >> "$INSTALLATION_LOG_FILE"
 {
   echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64"
   echo "export MAVEN_HOME=/usr/share/maven"
   echo "export PATH=$PATH:$JAVA_HOME/bin:$MAVEN_HOME/bin"
 } >> ~/.bashrc
 #Update env var changes
+# shellcheck disable=SC1090
 . ~/.bashrc
 else
-echo "Env var exists for openjdk-17-jdk maven" >> "$INSTALLATION_LOG_FILE"
+echo "$(date +'%Y-%m-%d %H:%M:%S') - Env var exists for openjdk-17-jdk maven" >> "$INSTALLATION_LOG_FILE"
 fi
 
-echo "Finished installing openjdk-17-jdk maven" >> "$INSTALLATION_LOG_FILE"
+echo "$(date +'%Y-%m-%d %H:%M:%S') - Finished installing openjdk-17-jdk maven" >> "$INSTALLATION_LOG_FILE"
