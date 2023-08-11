d3c584bd64f6b26540ba81ccc6870037fc34e684
Adarsh Singhal <adarsh.singhal@outlook.com>
2023-08-11 17:48:24 +0530

    Ubuntu native jdk installation (#5)
    Co-authored-by: Adarsh Singhal <adarshsinghal695@gmail.com>

--- server/ubuntu/native/nnt/installation-jdk-mvn.sh
+++ server/ubuntu/native/nnt/installation-jdk-mvn.sh
@@ -2,13 +2,17 @@
 echo "Installing openjdk-17-jdk maven" >> "$INSTALLATION_LOG_FILE"
 sudo apt install openjdk-17-jdk maven -y
 
+if ! grep -q "export JAVA_HOME" ~/.bashrc; then
+echo "Setting env var for openjdk-17-jdk maven" >> "$INSTALLATION_LOG_FILE"
 {
   echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64"
-  echo "export PATH=$PATH:$JAVA_HOME/bin"
   echo "export MAVEN_HOME=/usr/share/maven"
-  echo "export PATH=$PATH:$MAVEN_HOME/bin"
+  echo "export PATH=$PATH:$JAVA_HOME/bin:$MAVEN_HOME/bin"
 } >> ~/.bashrc
-
+#Update env var changes
 . ~/.bashrc
+else
+echo "Env var exists for openjdk-17-jdk maven" >> "$INSTALLATION_LOG_FILE"
+fi
 
 echo "Finished installing openjdk-17-jdk maven" >> "$INSTALLATION_LOG_FILE"
