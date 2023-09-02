c8d9b5e3bfcf0e318970d24039f96e37972a1905
Adarsh Singhal <adarsh.singhal@outlook.com>
2023-09-02 09:38:23 +0530

    Updated aws nnt scripts (#22)
    Co-authored-by: Adarsh Singhal <adarshsinghal695@gmail.com>

---
+++ server/ubuntu/aws/nnt/009-installation-hashicorp-vault.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+######################### Logging setup #########################
+LOG_DIR=/var/log/my_script_logs
+if ! [ -d "$LOG_DIR" ]; then
+  mkdir $LOG_DIR
+fi
+
+log() {
+  echo "$(date +'%Y-%m-%d %H:%M:%S') - $1" >> $LOG_DIR/installation.log
+}
+######################### Logging setup #########################
+
+log "*** Started execution - installation-hashicorp-vault.sh ***"
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install vault
+log "*** Finished execution - installation-hashicorp-vault.sh ***"
